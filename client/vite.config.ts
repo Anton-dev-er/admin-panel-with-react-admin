065eef3e9725d9a06285dfc831c0e931f83e63b9
Anton Plysiuk <78557277+Anton-dev-er@users.noreply.github.com>
2024-10-03 13:27:32 +0300

    setup docker for client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: "/",
   plugins: [react()],
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://localost:8080",
+  },
+  esbuild: {
+    loader: 'tsx',
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.ts': 'tsx',
+      },
+    },
+  },
+});
